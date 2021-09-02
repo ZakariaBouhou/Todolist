e01c18b11172e3f6752e19e72013deb58226b64d
ZakariaBouhou <zakbouhou1990@gmail.com>
2021-09-02 15:45:14 +0200

    form created + connected to db + insert tasks in db with post

---
+++ bdd.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '../../vendor/autoload.php';
+
+$db = new PDO("mysql:host=localhost;dbname=todolist", 'root', '');
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if (isset($_POST['creer_tache'])) { // On vérifie que la variable POST existe
+  if (empty($_POST['creer_tache'])) {  // On vérifie qu'elle as une valeure
+      $erreurs = 'Vous devez indiquer la valeure de la tâche';
+  } else {
+      $task = $_POST['creer_tache'];
+      $db->exec("INSERT INTO todo(task) VALUES('$task')"); // On insère la tâche dans la base de donnée
+  }
+}
+
+?>
